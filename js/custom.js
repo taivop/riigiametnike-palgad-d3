d95c7928708b45fa25f0ceff154133e801d80434
taivop <taivo.pungas@gmail.com>
2015-08-31 21:31:05 +0300

    work on #3 but piece of shit

--- js/custom.js
+++ js/custom.js
@@ -25,7 +25,7 @@ dsv("data/palgad.csv", function(d) {
     ready(rows);
 })
 
-ready = function(nodes) {
+ready = function(nodes_all) {
 
     var w = window,
         d = document,
@@ -34,10 +34,7 @@ ready = function(nodes) {
         width = w.innerWidth || e.clientWidth || g.clientWidth,
         height = 0.9 * (w.innerHeight|| e.clientHeight|| g.clientHeight);
 
-    nodes = nodes.filter(function(d) { return d.Tüüp == "Riigiasutus";})
-    nodes = getRandomSubarray(nodes, 1000);
-
-    asutused = d3.set(nodes.map(function(d) { return d.Asutus; })).values();
+    asutused = d3.set(nodes_all.map(function(d) { return d.Asutus; })).values();
 
     // Create foci
     foci = {}
@@ -50,84 +47,144 @@ ready = function(nodes) {
         }
     }
 
-    //var fill = d3.scale.category20b();
-    var salaryDomain =  [300, 600, 1000, 2000, 6000];
-    var fill = d3.scale.quantile()
-        .domain(salaryDomain)
-        .range(["#d7191c", "#fdae61", "#ffffbf", "#a6d96a", "#1a9641"].reverse()); // red-green
-    //.range(["#fff7ec", "#fc8d59", "#d7301f", "#b30000", "#7f0000"]); // white-red
+    var createPlot = function(nodes) {
 
-    var sizeScale = d3.scale.quantile()
-        .domain(salaryDomain)
-        .range([1, 1, 1, 2, 3]);
+        //var fill = d3.scale.category20b();
+        var salaryDomain =  [300, 600, 1000, 2000, 6000];
+        fillScale = d3.scale.quantile()
+            .domain(salaryDomain)
+            .range(["#d7191c", "#fdae61", "#ffffbf", "#a6d96a", "#1a9641"].reverse()); // red-green
+        //.range(["#fff7ec", "#fc8d59", "#d7301f", "#b30000", "#7f0000"]); // white-red
 
-    var force = d3.layout.force()
-        .nodes(nodes)
-        .size([width, height])
-        .charge(-15)
-        .on("tick", tick)
-        .start();
+        sizeScale = d3.scale.quantile()
+            .domain(salaryDomain)
+            .range([1, 1, 1, 2, 3]);
 
-    var svg = d3.select("body").append("svg")
-        .attr("width", width)
-        .attr("height", height)
+        force = d3.layout.force()
+            .nodes(nodes)
+            .size([width, height])
+            .charge(-15)
+            .on("tick", tick)
+            .start();
 
-    var node = svg.selectAll(".node")
-        .data(nodes)
-        .enter().append("circle")
-        .attr("class", "node")
-        .attr("cx", function(d) { return Math.random() * width; })
-        .attr("cy", function(d) { return Math.random() * width; })
-        .attr("r", function(d) {return sizeScale(d.Palk)})
-        .style("fill", function(d, i) { return fill(d.Palk); })
-        //  .style("stroke", function(d, i) { return d3.rgb(fill(d.Palk)).darker(2); })
-        .call(force.drag)
-        .on("mousedown", function() { d3.event.stopPropagation(); });
+        var svg = d3.select("body").append("svg")
+            .attr("width", width)
+            .attr("height", height)
 
-    var infobar = d3.select("#infobar");
+        var node = svg.selectAll(".node")
+            .data(nodes)
+            .enter().append("circle")
+            .attr("class", "node original")
+            .attr("cx", function(d) { return Math.random() * width; })
+            .attr("cy", function(d) { return Math.random() * width; })
+            .attr("r", function(d) {return sizeScale(d.Palk)})
+            .style("fill", function(d, i) { return fillScale(d.Palk); });
 
-    svg
-        .on("mousedown", mousedown);
+        var infobar = d3.select("#infobar");
 
-    node.on("mouseenter", function(d) {
-        // Asutus
-        infobar.select("#asutus")
-            .text(d.Asutus)
-        // Ametikoht
-        infobar.select("#ametikoht")
-            .text(d.Ametikoht)
-        // Palk
-        infobar.select("#palk")
-            .text(d.Palk + "€")
-            .style("color", fill(d.Palk))
-        // Nimi
-        if(d.Nimi == "NA")
-            infobar.select("#nimi").text("---")
-        else
-            infobar.select("#nimi").text(d.Nimi)
-    })
+        mousedown = function() {
+            nodes.forEach(function(o, i) {
+                o.x += (Math.random() - .5) * 40;
+                o.y += (Math.random() - .5) * 40;
+            });
+            force.resume();
+        }
 
-    function tick(e) {
-        var k = 1 * e.alpha;
+        mouseover = function(d) {
+            // Asutus
+            infobar.select("#asutus")
+                .text(d.Asutus)
+            // Ametikoht
+            infobar.select("#ametikoht")
+                .text(d.Ametikoht)
+            // Palk
+            infobar.select("#palk")
+                .text(d.Palk + "€")
+                .style("color", fillScale(d.Palk))
+            // Nimi
+            if(d.Nimi == "NA")
+                infobar.select("#nimi").text("---")
+            else
+                infobar.select("#nimi").text(d.Nimi)
+        }
 
-        // Push nodes toward their designated focus.
-        nodes.forEach(function(o, i) {
-            focus = foci[o.Asutus]
-            o.y += (focus.y_rel * height - o.y) * k;
-            o.x += (focus.x_rel * width - o.x) * k;
-        });
+        svg
+            .on("mousedown", mousedown);
 
-        node
-            .attr("cx", function(d) { return d.x; })
-            .attr("cy", function(d) { return d.y; });
+        svg.selectAll(".node").on("mouseenter", mouseover);
+
+        function tick(e) {
+            var k = 1 * e.alpha;
+
+            // Push nodes toward their designated focus.
+            nodes.forEach(function(o, i) {
+                focus = foci[o.Asutus]
+                o.y += (focus.y_rel * height - o.y) * k;
+                o.x += (focus.x_rel * width - o.x) * k;
+            });
+
+            svg.selectAll(".node")
+                .attr("cx", function(d) { return d.x; })
+                .attr("cy", function(d) { return d.y; });
+        }
+
+        d3.select("#asutus_filter")
+            .on("change", function() {
+                var filter_word = d3.event.target.value;
+
+                // Subset nodes accordingly
+                if(filter_word == "kõik")
+                    nodes_new = nodes_all;
+                else if(filter_word == "muud")
+                    nodes_new = nodes_all.filter(function(d) {
+                        return d.Asutus.indexOf("Maavalitsus") == -1 &&
+                            d.Asutus.indexOf("ministeerium") == -1 &&
+                            d.Asutus.indexOf("kohus") == -1;
+                    })
+                else
+                    nodes_new = nodes_all.filter(function(d) { return d.Asutus.indexOf(filter_word) > -1; })
+
+                // If too many nodes, sample
+                if(nodes_new.length >= 1000)
+                    nodes_new = getRandomSubarray(nodes_new, 1000);
+
+                // Apply change
+                //changeNodes(nodes_new);
+            })
+
     }
 
-    function mousedown() {
-        nodes.forEach(function(o, i) {
-            o.x += (Math.random() - .5) * 40;
-            o.y += (Math.random() - .5) * 40;
-        });
-        force.resume();
+    var changeClusters = function() {
+        // create new foci
+        // rearrange existing nodes
+    }
+
+    var changeNodes = function(nodes) {
+
+        console.log(nodes.length + "nodes, width " + width)
+        var duration = 100;
+
+        var node = d3.select("svg").selectAll(".node")
+            .data(nodes)
+
+        node.enter()
+            .append("circle")
+            .attr("cx", function(d) { return Math.random() * width; })
+            .attr("cy", function(d) { return Math.random() * width; })
+            .attr("class", "node entered")
+
+        node
+            .style("fill", function(d) { return fillScale(d.Palk); })
+            .attr("r", function(d) {return sizeScale(d.Palk)})
+
+        node.exit()
+            .remove()
+
+        d3.selectAll(".node")
+            .on("mouseenter", mouseover);
+
+        force.resume()
+
     }
 
     function updateWindow() {
@@ -151,5 +208,8 @@ ready = function(nodes) {
 
         force.resume()
     }
+
+    createPlot(getRandomSubarray(nodes_all, 1000).filter(function(d) { return d.Tüüp == "Riigiasutus";}));
+
     //window.onresize = updateWindow;
 }
